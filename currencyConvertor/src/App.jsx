a989335a4477d860d2c273c7ecbff981c37f381d
iApurvJha <apurvjha302@gmail.com>
2024-04-21 10:32:14 +0530

    Add currency swap feature

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -13,11 +13,16 @@ function App() {
   function getRes(){
     GetCurrency({currencyFirst},{givenVal},{currencySecond}, {setConvertedVal})
   }
+  function currChange(){
+    setCurrencyFirst(currencySecond)
+    setCurrencySecond(currencyFirst)
+  }
   return (
     <>
      <div className='center'>
       <div className='container'>
         <Input type="number" label1="from" label2="currency Type" readOnly={false} currency={currencyFirst} setCurrency={setCurrencyFirst} Val={setVal} curr={givenVal}/>
+        <button onClick={currChange} className='swap-cnvt'>SWAP</button>
         <Input  label1="To" label2="currency Type " readOnly={true}  currency={currencySecond} setCurrency={setCurrencySecond} val={setConvertedVal} curr={convertedVal}  />
         <button onClick={getRes} className='btn-cnvt'>Convert  {currencyFirst} To {currencySecond}</button>
       </div>
